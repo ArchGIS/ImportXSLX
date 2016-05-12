7e0ba9b501b572bf063574bffdfa456f2c73078d
quasilyte <quasilyte@gmail.com>
2016-05-12 12:47:47 +0300

    xl пакет для упрощения работы с xlsx

--- src/xl/table.go
+++ src/xl/table.go
@@ -1,5 +1,54 @@
 package xl
 
-func NewTable(xlsxFilePath string) (*Table, error) {
+import (
+	"github.com/tealeg/xlsx"
+)
 
+func toString(cell *xlsx.Cell) string {
+	stringValue, err := cell.String()
+	if err != nil {
+		// Не знаю, что здесь за ошибка может быть, лень
+		// смотреть исходники функции xlsx.Cell.String(). Возможно она всегда nil
+		// ошибку возвращает. Это можно проверить как-нибудь потом.
+		panic(err)
+	}
+
+	return stringValue
+}
+
+func NewTable(xlsxFilePath string) (*Table, error) {
+	file, err := xlsx.OpenFile(xlsxFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	xlsxSheet := file.Sheets[0]     // Работаем только с первым листом
+	xlsxHeader := xlsxSheet.Rows[0] // Первый ряд - заголок
+	xlsxRows := xlsxSheet.Rows[1:]  // Остальные - данные
+
+	// Подготавливаем header для Table
+	header := make(Header, len(xlsxHeader.Cells))
+	for index, cell := range xlsxHeader.Cells {
+		header[index] = toString(cell)
+	}
+
+	// Подготавливаем rows для Table
+	rows := make([]Row, len(xlsxRows))
+	for rowIndex, xlsxRow := range xlsxRows {
+		row := Row{
+			Header: &header,
+			Cells:  make([]string, len(header)),
+		}
+
+		for cellIndex, cell := range xlsxRow.Cells {
+			row.Cells[cellIndex] = toString(cell)
+		}
+
+		rows[rowIndex] = row
+	}
+
+	return &Table{
+		Header: header,
+		Rows:   rows,
+	}, nil
 }
--- src/xl/types.go
+++ src/xl/types.go
@@ -3,11 +3,13 @@ package xl
 type Header []string
 
 type Row struct {
+	// Тут не подойдёт map[string]string потому что нам важен порядок
+	// элементов в ряду. Map в Go имеет рандомный порядок обхода.
 	Header *Header
 	Cells  []string
 }
 
 type Table struct {
 	Header Header
-	rows   []Row
+	Rows   []Row
 }
